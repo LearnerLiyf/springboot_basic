6ca52e81249306c2646bd51c3f937b292391cca4
LearnerLiyf <3228167904@qq.com>
2023-07-18 16:52:15 +0800

    搭建一个简单的跑得起来的springboot项目

---
+++ src/test/java/com/dolphis/service/BookServiceTest.java
@@ -0,0 +1,24 @@
+package com.dolphis.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class BookServiceTest {
+
+    @Autowired
+    private BookService bookService;
+
+    @Test
+    public void testGetById(){
+        System.out.println(bookService.getById(1));
+    }
+
+    @Test
+    public void testGetAll(){
+        System.out.println(bookService.getAll());
+    }
+}
